e7745aed91bc4cbcf9689e0822ff5f067aa251b1
AbdouMatrix <abdelrahman_hassan93@outlook.com>
2025-08-17 12:30:33 +0300

    Update config.js

--- Core/config.js
+++ Core/config.js
@@ -1,8 +1,8 @@
 ﻿export const CONFIG = {
     MAX_DBR_RATIO: 50,
-    MIN_INTEREST_RATE: 22.75,
+    MIN_INTEREST_RATE: 22.5,
     PRESET_RATES: {
-        'min': 22.75,
+        'min': 22.5,
         'private': 24,
         'digital': 24.50
     },
